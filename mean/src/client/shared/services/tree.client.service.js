b9a8707fcc5966f6d7d375ed369fbce54d7dd943
Trịnh Quốc Hoàng <quochoangvp@gmail.com>
2017-09-28 11:19:42 +0700

    Fix group tree sort

--- mean/src/client/shared/services/tree.client.service.js
+++ mean/src/client/shared/services/tree.client.service.js
@@ -13,6 +13,10 @@
               var childGroups = _.filter(groups, function(group) {
                 return (!group.parent && !rootId) || group.parent === rootId;
               });
+              childGroups = _.sortBy(childGroups, function (childGroup) {
+                return childGroup.order;
+              });
+
               var fullTitle = root ? root.fullTitle : '';
               var childNodes = _.map(childGroups, function(obj) {
                 return {
@@ -29,6 +33,11 @@
                 childNode.children = buildTopDownTree(childNode.key, childNode);
                 childNode.parent = root;
               });
+
+              childNodes = _.sortBy(childNodes, function (childNode) {
+                return childNode.data.order;
+              });
+
               return childNodes;
             }
             return buildTopDownTree(null, null);
