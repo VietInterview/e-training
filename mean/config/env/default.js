157dd7154e1a8d66c8ecb47aa14b7b6ddeaa2ee8
viethoang.0707 <viethoang.070793@gmail.com>
2017-12-19 11:31:19 +0700

    update config

--- mean/config/env/default.js
+++ mean/config/env/default.js
@@ -3,7 +3,7 @@
 var s = module.exports = {
   app: {
     title: 'e-Training',
-    customer: "Aten",
+    customer: "Vietinbank",
     description: 'Corporate Learning Management System',
     keywords: 'Training, LMS, CMS, Enterprise',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
